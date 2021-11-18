577350f164586bc13a1d9d5d2b1e486b7c7316bf
Tskar <sanskarthapaofficial@gmail.com>
2021-11-18 16:41:54 -0500

    light-dark toggler update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import {faTrash} from '@fortawesome/free-solid-svg-icons';
 import {faCheck} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled, {ThemeProvider} from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 
 
 //using button designs from fontAwesome
@@ -13,6 +15,54 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 library.add(faPlus);
 library.add(faTrash);
 library.add(faCheck);
+
+
+//Styled container
+const Container = styled.div`
+  text-align: center;
+  justify-content: center;
+`;
+
+
+//Global style to switch attributes features
+const GlobalStyles = createGlobalStyle`
+body {
+  background-color: ${props => props.theme.body};
+  color: ${props => props.theme.text};
+  transition: 1s;
+}
+`;
+
+//Styled buttons
+const Buttons = styled.button`
+  color: ${props => props.theme.buttonColor};
+  transition: 1s;
+`;
+
+//Styled input fields
+const Input = styled.input`
+  background: ${props => props.theme.inputColor};
+`;
+//Styled option fields
+const Select = styled.select`
+  background: ${props => props.theme.inputColor};
+`;
+
+//toggle characteristics for dark and light mode
+const lightTheme = {
+  body: 'rgb(243, 217, 217)',
+  text: 'rgb(0, 0, 0)',
+  inputColor: 'white',
+  buttonColor: 'rgb(0, 0, 0)'
+};
+
+const darkTheme = {
+  body: "#121212",
+  text: 'rgb(243, 217, 217)',
+  inputColor: 'rgb(243, 217, 217)',
+  buttonColor: 'rgb(243, 217, 217)'
+}
+
 
 function App() {
 
@@ -22,7 +72,7 @@ function App() {
   const [listObjects, setListObjects] = useState([]);
   const [status, setStatus] = useState("items");
   const [editedList, setEditedList] = useState([]);
-
+  const [theme, setTheme] = useState("light");
 
   //useEffect to update everytime status is changed
   useEffect (() => {
@@ -32,9 +82,7 @@ function App() {
 
   //adress new item input
   const newItemHandler = (e) => {
-    if(e.target.value.length > 0) {
       setInputText(e.target.value);
-    }
   }
 
   //adress the quantity
@@ -49,9 +97,12 @@ function App() {
     { text: inputText,
       quantity: inputQuantity,
       picked: false,
+      removed: false,
       id: Math.random() * 1000
     }]);
-    setInputText("");
+    setInputText('');
+    setInputQuantity(1);
+  
   };
 
   //filter the list based on picked or not.
@@ -74,33 +125,46 @@ function App() {
     setStatus(e.target.value);
   };
 
+  const toggleHandler =() => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  }
+
   return (
-    <div className="App">
-      <header>
-        <h1>.list</h1>
-      </header>
-      <form>
-        <input className = "input-text-box" type = "text" placeholder = "item" onChange={newItemHandler}/>
-        <input className = "quantity-box" type = "number" placeholder = "1" min="1" onChange={itemQuantityHandler} />
-        <button className = "add-button" type = "submit" onClick={addItemHandler}>
-          <i>
-            <FontAwesomeIcon className="faicons-plus" icon='plus' />
-          </i>
-        </button>     
-        
-        <div className="option-select">
-          <select className="list-options" onChange={statusHandler}>
-            <option value="items">items</option>
-            <option value="picked">picked items</option>
-            <option value="unpicked">unpicked items</option>
-          </select>
-        </div>
-      </form>
-      <List listObjects={listObjects}
-            setListObjects={setListObjects}
-            editedList={editedList}
-            />
-    </div>
+    <ThemeProvider theme={theme === "light"? lightTheme: darkTheme}>
+      <GlobalStyles />
+      <Container>
+          <header>
+            <h1>.list</h1>
+          </header>
+          <form>
+            <Input className = "input-text-box" type = "text" value={inputText} placeholder = "item" onChange={newItemHandler}/>
+            <Input className = "quantity-box" type = "number" value = {inputQuantity} placeholder = "1" min="1" onChange={itemQuantityHandler} />
+            <Buttons className = "add-button" type = "submit" onClick={addItemHandler}>
+              <i>
+                <FontAwesomeIcon className="faicons-plus" icon='plus' />
+              </i>
+            </Buttons>     
+            
+            <div className="option-select">
+              <Select className="list-options" onChange={statusHandler}>
+                <option value="items">items</option>
+                <option value="picked">picked items</option>
+                <option value="unpicked">unpicked items</option>
+              </Select>
+            </div>
+            <div>
+            <label className="switch">
+              <input type="checkbox" onClick={toggleHandler} />
+              <span className="toggler"/>
+            </label>
+            </div>
+          </form>
+          <List listObjects={listObjects}
+                setListObjects={setListObjects}
+                editedList={editedList}
+                />
+      </Container>
+    </ThemeProvider> 
   );
 };
 
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from 'styled-components';
 
+
+const Buttons = styled.button`
+  color: ${props => props.theme.buttonColor};
+`;
 
 function Item({text, quantity, listObject, listObjects, setListObjects}) {
 
     //handle removed items
     const removeHandler = () => {
         setListObjects(listObjects.filter((item) => item.id !== listObject.id));
+            
     };
 
     //handle picked items
@@ -26,16 +32,16 @@ function Item({text, quantity, listObject, listObjects, setListObjects}) {
             <li className={`item-to-pick ${listObject.picked ? "picked": ''}`}>
                 {quantity} {text}
             </li>
-            <button className="picked-button" onClick={pickedHandler}>
+            <Buttons className="picked-button" onClick={pickedHandler}>
                 <i>
                     <FontAwesomeIcon className="faicons-check" icon='check'/>
                 </i>
-            </button>
-            <button className="remove-button" onClick={removeHandler}>
+            </Buttons>
+            <Buttons className="remove-button" onClick={removeHandler}>
                 <i>
                     <FontAwesomeIcon className="faicons-trash" icon='trash'/>
                 </i>
-            </button>
+            </Buttons>
 
         </div>
     );
